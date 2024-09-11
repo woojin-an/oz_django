945fd493786787044c203f056e32e91f0db94671
woojin-an <dkswoojin@gmail.com>
2024-09-11 11:58:27 +0900

    implement prefetch

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,5 +1,11 @@
+from django.db.models import QuerySet
+
 from tabom.models import Article
 
 
 def get_an_article(article_id: int) -> Article:
     return Article.objects.get(id=article_id)
+
+
+def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+    return Article.objects.order_by("-id").prefetch_related("like_set")[offset : offset + limit]
--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -1,7 +1,9 @@
+from django.db import connection
 from django.test import TestCase
+from django.test.utils import CaptureQueriesContext
 
-from tabom.models import Article
-from tabom.services.article_service import get_an_article
+from tabom.models import Article, Like, User
+from tabom.services.article_service import get_an_article, get_article_list
 
 
 class TestArticleService(TestCase):
@@ -21,3 +23,17 @@ class TestArticleService(TestCase):
         # Expect
         with self.assertRaises(Article.DoesNotExist):
             get_an_article(invalid_article_id)
+
+    def test_get_article_list_should_prefetch_likes(self) -> None:
+        # Given
+        user = User.objects.create(name="test_user")
+        articles = [Article.objects.create(title=f"test_title_{i}") for i in range(1, 21)]
+        Like.objects.create(user_id=user.id, article_id=articles[-1].id)
+
+        with self.assertNumQueries(2):
+            result_articles = get_article_list(0, 10)
+            result_counts = [a.like_set.count() for a in result_articles]
+
+            self.assertEqual(len(result_articles), 10)
+            self.assertEqual(1, result_counts[0])
+            self.assertEqual([a.id for a in reversed(articles[10:21])], [a.id for a in result_articles])
